cbd012f07d2fcf065f9086e074e509c64e9d6ae8
bennettapps <jb.jediknight@gmail.com>
2019-12-30 16:18:20 -0800

    Started building some basic functionality. Clicking "plus" in the home tab adds a new category.

--- Budget/ViewController.swift
+++ Budget/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var list: [String] = []
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if(editingStyle == UITableViewCell.EditingStyle.delete)
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func onHomeButtonPressed(_ sender: Any) {
-        print("YESSS!")
+    
+    @IBAction func homePlusButtonClicked(_ sender: Any) {
+        list.append("NEW CATEGORY")
+        myTableView.reloadData()
     }
 }
-
