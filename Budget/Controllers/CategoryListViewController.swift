02070bf74b4ae0d0b9c71aef64661205acf1d9d1
bennettapps <jb.jediknight@gmail.com>
2020-01-22 13:42:53 -0800

    Implement Autocorrect, action sheet when clicking a category, and editing categories

--- Budget/Controllers/CategoryListViewController.swift
+++ Budget/Controllers/CategoryListViewController.swift
@@ -13,7 +13,7 @@ class CategoryListViewController: UIViewController, UITableViewDelegate, UITable
     let realm = try! Realm()
     
     var categoryNames: Results<Category>?
-
+    
     @IBOutlet weak var myTableView: UITableView!
     
     override func viewDidLoad() {
@@ -38,19 +38,24 @@ class CategoryListViewController: UIViewController, UITableViewDelegate, UITable
     {
         if(editingStyle == UITableViewCell.EditingStyle.delete)
         {
-            try? self.realm.write ({
-                realm.delete((categoryNames?[indexPath.row])!)
-            })
-            myTableView.reloadData()
+            deleteRow(row: indexPath.row)
         }
     }
     
+    func deleteRow(row: Int) {
+        try? self.realm.write ({
+            realm.delete((categoryNames?[row])!)
+        })
+        myTableView.reloadData()
+    }
+    
     @IBAction func homePlusButtonClicked(_ sender: Any) {
-        let alert = UIAlertController(title: "New Category", message: "", preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "New Category", message: nil, preferredStyle: UIAlertController.Style.alert)
         
         alert.addTextField(configurationHandler: nil)
         alert.textFields![0].placeholder = "Enter a name..."
-        
+        alert.textFields![0].autocorrectionType = .yes
+
         alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
         
         alert.addAction(UIAlertAction(title: "Create", style: UIAlertAction.Style.default, handler: {(action) in
@@ -77,9 +82,44 @@ class CategoryListViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let alert = UIAlertController(title: "Move", message: "", preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertController.Style.actionSheet)
         
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {(action) in
+            self.deleteRow(row: indexPath.row)
+        }))
+        alert.addAction(UIAlertAction(title: "Transfer", style: .default, handler: {(action) in
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Edit", style: .default, handler: {(action) in
+            let alert = UIAlertController(title: "Edit Category", message: nil, preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields![0].placeholder = "Enter a name..."
+            alert.textFields![0].text = self.categoryNames![indexPath.row].title
+            alert.textFields![0].autocorrectionType = .yes
+            
+            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
+            
+            alert.addAction(UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: {(action) in
+                if(alert.textFields![0].hasText) {
+                    let newCategory = self.categoryNames![indexPath.row]
+                    do {
+                        try self.realm.write {
+                            newCategory.title = alert.textFields![0].text!
+                        }
+                    } catch {
+                        print("Error saving category \(error)")
+                    }
+                    
+                    self.myTableView.reloadData()
+                }
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {(action) in
+            
+        }))
         
         self.present(alert, animated: true, completion: nil)
         
